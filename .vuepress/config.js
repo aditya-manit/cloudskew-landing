27fd08761f9765b0b6218fe4a944a0f17015562d
mithun shanbhag <mithunshanbhag@hotmail.com>
2020-02-28 12:06:50 +0530

    prepping navbar + homepage

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,35 @@
 module.exports = {
-    title: 'Hello VuePress!!!',
-    description: 'Just playing around'
+  title: 'CloudSkew',
+  description: 'Free online diagram editor for creating cloud architecture diagrams',
+  themeConfig: {
+    // logo: '/assets/cloudskew-logo.png',
+    nav: [
+      { text: 'Blog', link: '/blog/' },
+      { text: 'Docs', link: '/docs/' },
+      { text: 'Login / Signup', link: 'https://app.cloudskew.com' },
+      { text: 'Pricing', link: '' },
+      { text: 'Status', link: '' },
+      {
+        text: 'About',
+        ariaLabel: 'About menu',
+        link: '/about',
+        items: [
+          {
+            text: 'Product', items: [
+              { text: 'Issues', link: 'https://github.com/cloudskew/cloudskew/issues' },
+              { text: 'Privacy Policy', link: '/about/privacy-policy' },
+              { text: 'Releases', link: 'https://github.com/cloudskew/cloudskew/releases' },
+            ],
+          },
+          {
+            text: 'Company', items: [
+              { text: 'Contact Us', link: '/about/contact-us' },
+            ],
+          },
+
+
+        ],
+      },
+    ]
   }
+}
